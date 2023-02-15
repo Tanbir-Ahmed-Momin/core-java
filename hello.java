d630c2d4e086d0c2338e742a84bf9cad5a43cf9b
Tanbir-Ahmed-Momin <tanbir15-12844@diu.edu.bd>
2023-02-15 20:44:33 +0600

    hello update

--- hello.java
+++ hello.java
@@ -1,6 +1,25 @@
 class hello{
 
     public static void main(String[] args) {
+
+        //print command-> output
         System.out.println("Hello java");
+
+        //diffent of print or println.
+        //print -> print without new line.
+        System.out.print("My name is ");
+        System.out.print("My name is Tanbir");
+
+        System.out.print('\n');//new line "\n"
+
+        //println -> print with new line
+        System.out.println("what is your name?");
+        System.out.println("My name is Tanbir");
+
+        //math calculation
+        System.out.println(5*6);
+
+
+
     }
 }
